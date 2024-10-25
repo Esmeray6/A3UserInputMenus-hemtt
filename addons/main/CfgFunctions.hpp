5fbe4fcd7d1b9a01503bc298f6c0586c42fb2858
Esmeray/Sky <22858781+Esmeray6@users.noreply.github.com>
2024-10-25 21:54:23 +0500

    This isn't necessary

--- addons/main/CfgFunctions.hpp
+++
@@ -1,7 +0,0 @@
-/*──────────────────────────────────────────────────────┐
-│   Author: Connor                                      │
-│   Steam:  https://steamcommunity.com/id/_connor       │
-│   Github: https://github.com/ConnorAU                 │
-│                                                       │
-│   Please do not modify or remove this comment block   │
-└──────────────────────────────────────────────────────*/
